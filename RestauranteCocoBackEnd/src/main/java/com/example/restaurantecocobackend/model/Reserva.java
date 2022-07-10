d1cbe5f897c438482a2d6c608786867eff82a567
Jeferson David Quisuruco Ciprian <i201920932@cibertec.edu.pe>
2022-07-10 15:31:28 -0500

    deploye 2

---
+++ RestauranteCocoBackEnd/src/main/java/com/example/restaurantecocobackend/model/Reserva.java
@@ -0,0 +1,25 @@
+package com.example.restaurantecocobackend.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Table(name="tblReserve")
+@Entity
+public class Reserva {
+
+      @Id
+      private  Integer id;
+      private  String direccion;
+      private  String fecha;
+      private  String cantidad;
+      private  String mesa;
+      @ManyToOne
+      @JoinColumn(name = "idUser")
+      private  Usuario use;
+
+
+}
